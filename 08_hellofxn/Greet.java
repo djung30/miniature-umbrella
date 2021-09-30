ab7efb85eaad904a03479c4475687ca47f454980
Daniel Jung <djung30@stuy.edu>
2021-09-30 14:05:30 -0400

    updated Greet w/ personalized mesages

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,10 @@
+public class Greet {
+ 	public static void main (String [] args) {
+		Greet2("Jones");
+		Greet2("Mack");
+		Greet2("Kevin");
+	}
+  	public static void Greet2 (String s) {
+  		System.out.println("Good morning "+s+", it is my pleasure to see you on this scrumptious day!");
+  	}
+}
