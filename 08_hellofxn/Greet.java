3133ef0ad6012f1c058015554779997f0f7059b5
Daniel Jung <djung30@stuy.edu>
2021-09-30 22:17:45 -0400

    Update Greet.java

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,10 +1,10 @@
 public class Greet {
  	public static void main (String [] args) {
-		Greet2("Jones");
-		Greet2("Mack");
-		Greet2("Kevin");
+		greet("Jones");
+		greet("Mack");
+		greet("Kevin");
 	}
-  	public static void Greet2 (String s) {
+  	public static void greet (String s) {
   		System.out.println("Good morning "+s+", it is my pleasure to see you on this scrumptious day!");
   	}
 }
