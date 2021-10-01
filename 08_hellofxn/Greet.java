ae33ac4c8cf0f55291375e2413596a628ce92ed7
Daniel Jung <djung30@stuy.edu>
2021-09-30 22:24:48 -0400

    Update Greet.java
    added comments and increased clarity

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -4,7 +4,16 @@ public class Greet {
 		greet("Mack");
 		greet("Kevin");
 	}
-  	public static void greet (String s) {
-  		System.out.println("Good morning "+s+", it is my pleasure to see you on this scrumptious day!");
+  	public static void greet (String name) {
+  		System.out.println("Good morning "+name+", it is my pleasure to see you on this scrumptious day!");
   	}
 }
+/* 
+	DISCOVERIES
+		You can create functions outside of the main code block and call upon them in mid
+		Method to allow a string input into the function that was then used as a name 
+
+	UNRESOLVED QUESTIONS
+		Why does activating javac not create a class file in the github repo?
+			Possibly because we are not pushing the class file to git? 
+*/
