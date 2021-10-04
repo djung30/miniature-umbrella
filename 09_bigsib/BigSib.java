2ea2e67f36431462ffa8038f1e0fb4f06ced2f89
Daniel Jung <djung30@stuy.edu>
2021-10-04 00:30:19 -0400

    Update BigSib.java

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -4,3 +4,8 @@ HW<09> -- <What Are BigSibs Good For?>
 <2021>-<10>-<2>
 */
 
+public class BigSib (String []) {
+    public static void greet (String name) {
+  	    System.out.println("Good morning "+name+", it is my pleasure to see you on this scrumptious day!");
+  	}
+}
