78ff9d15584e729833aa474b2c0e8b5de8136806
Daniel Jung <djung30@stuy.edu>
2021-10-05 00:38:19 -0400

    Update BigSib.java

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,21 @@
+/* <Dunking Ducks> (Humans: Daniel, Jason, Julia; Ducks: Ralph, Tiggy, Flopsy)
+APCS
+HW<10> -- <Refactor Big Sib One / >
+
+<2021>-<10>-<4>
+
+DISCOVERIES
+Our goal is to make a method called greet that will intake the string and instead of printing it, 
+it will rather return it so that we can print it in the main function
+
+We use a different beginning of the method, specifiying the type of data it is outputting 
+
+UNRESOLVED QUESTIONS
+
+*/
+
+public class BigSib {
+    public static String greet(String name) {
+  	    return "Good morning "+name+", it is my pleasure to see you on this scrumptious day!";
+  	}
+}
