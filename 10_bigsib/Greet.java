de2278e06fc7dae9d3926ffeb9c1aa61987e101a
Daniel Jung <djung30@stuy.edu>
2021-10-05 00:33:10 -0400

    Update Greet.java

---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,7 @@
+public class Greet {
+    public static void main( String[] args ){
+        System.out.println( BigSib.greet("Crosby") );
+        System.out.println( BigSib.greet("Stills") );
+        System.out.println( BigSib.greet("Nash") );
+    }
+}
