73618d060329aa89193fb728ce1e9d7e8f1dc9f5
Daniel Jung <djung30@stuy.edu>
2021-10-05 23:16:30 -0400

    increase

--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -8,5 +8,23 @@ public class Greet {
     greeting = richard.greet("freshman");
     System.out.println(greeting);
 
+    BigSib prichard = new BigSib();
+    prichard.setHelloMsg("Salutations");
+
+    greeting = prichard.greet("Dr. Spaceman");
+    System.out.println(greeting);
+
+    BigSib brichard = new BigSib();
+    richard.setHelloMsg("Hey ya");
+
+    greeting = brichard.greet("Kong Fooey");
+    System.out.println(greeting);
+
+    BigSib qrichard = new BigSib();
+    richard.setHelloMsg("Sup");
+
+    greeting = qrichard.greet("mom");
+    System.out.println(greeting);
+
   } //end main()
 } //end Greet
