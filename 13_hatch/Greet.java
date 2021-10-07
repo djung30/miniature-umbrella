8f02b73b4f8e3edddd558f616be476ac5e26464c
Daniel Jung <djung30@stuy.edu>
2021-10-06 22:01:45 -0400

    update

--- 13_hatch/Greet.java
+++ 13_hatch/Greet.java
@@ -2,16 +2,16 @@ public class Greet {
   public static void main( String[] args ) {
     String greeting;
 
-    BigSib richard = new BigSib("Word up");
+    BigSib richard = new setHelloMsg("Word up");
     System.out.println(richard.greet("freshman"));
 
-    BigSib prichard = new BigSib("Salutations");
+    BigSib prichard = new setHelloMsg("Salutations");
     System.out.println(prichard.greet("Dr. Spaceman"));
 
-    BigSib brichard = new BigSib("Hey ya");
+    BigSib brichard = new setHelloMsg("Hey ya");
     System.out.println(brichard.greet("Kong Fooey"));
 
-    BigSib qrichard = new BigSib("Sup");
+    BigSib qrichard = new setHelloMsg("Sup");
     System.out.println(qrichard.greet("mom"));
 
   } //end main()
