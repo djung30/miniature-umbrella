13e47adc3dd512c65ed0406fa92f7395d3185140
Daniel Jung <djung30@stuy.edu>
2021-10-06 22:20:05 -0400

    update

--- 13_hatch/BigSib.java
+++ 13_hatch/BigSib.java
@@ -8,13 +8,17 @@ QCC
 	 Still cannot exaclty wrap my head around exact details about constructors
 */
 public class BigSib {
+
 	private String msg;
+
 	public BigSib(String phrase) {
 		setMsg(phrase);
 	}
+
 	public void setMsg(String phrase) {
 		msg = phrase;
 	}
+	
 	public String greet(String name) {
 		return msg + " " + name; 
 	}
