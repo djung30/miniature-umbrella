0c41e700cd29b77fd723904cc3cc706673009c8f
Daniel Jung <djung30@stuy.edu>
2021-10-06 21:39:10 -0400

    Update BigSib.java

---
+++ 13_hatch/BigSib.java
@@ -0,0 +1,20 @@
+/* Tom & Jerry (Humans: Daniel, Justin; Ducks: Ralph, Alfred)
+APCS
+HW 12 -- On Elder Individuality and the Elimination of Radio Fuzz
+2021 10 05
+DISCOVERIES
+	We can refrence methods from different files 
+	We can store information using methods from different files 
+UNRESOLVED QUESTIONS
+	What does static do/mean? 
+*/
+
+public class BigSib {
+	String msg;
+	public void setHelloMsg(String phrase) {
+		msg = phrase;
+	}
+	public String greet(String name) {
+		return msg + " " + name; 
+	}
+}
