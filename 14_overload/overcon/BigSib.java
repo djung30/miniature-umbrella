006cf9622a31b9823828a7c7b73cd58c94c6e6b0
Daniel Jung <djung30@stuy.edu>
2021-10-07 23:35:05 -0400

    heading

--- 14_overload/overcon/BigSib.java
+++ 14_overload/overcon/BigSib.java
@@ -1,3 +1,13 @@
+/* Tom & Jerry & Jason (Humans: Daniel, Justin, Jason; Ducks: Ralph, Alfred, Tiggy)
+APCS
+HW 14 -- Customize Your Creation
+2021 10 07
+DISCO
+	You can "overload" a constructor with variations of different parameters in order to take different values of inputs. 
+QCC
+	
+*/
+
 public class BigSib {
 
 	private String msg;
