6532cef72efb17e6e0dd7965602be13516dee9da
Daniel Jung <djung30@stuy.edu>
2021-10-19 14:05:38 -0400

    update

--- BreakingTheBank/Teller.java
+++ BreakingTheBank/Teller.java
@@ -1,1 +1,13 @@
-
+public class Teller{
+	public static void main( String[] args ) {
+	BankAccount test = new BankAccount();
+	test.setName("Tater Tots");
+	test.setPasswd("Taters123456");
+	test.setPin((short) 12345);
+	test.AcctNum(123456789);
+	test.setBalance(600000000000000000000000000000000000000000000.00);
+	test.deposit(400000000000000000000000000000);
+	test.withdraw(10000000000000000000000000000000);
+	System.out.println(test.toString());
+	}
+}
