bf37be6708efab5f22d68d8fc97c00eb2eb99d88
Daniel Jung <djung30@stuy.edu>
2021-10-19 23:51:52 -0400

    p

--- 20_whitehat/Teller.java
+++ 20_whitehat/Teller.java
@@ -25,15 +25,15 @@ public class Teller{
 	System.out.println(test.toString());
 
 	// Breaking The Bank
-	BankAccount test = new BankAccount();
-	test.setName("Tater Tots");
-	test.setPasswd("Taters123456");
-	test.setPin((short) 12345);
-	test.AcctNum(123456789);
-	test.setBalance(1);
-	test.deposit(01000);
-	test.withdraw(80);
-	System.out.println(test.toString());
+	BankAccount hack = new BankAccount();
+	hack.setName("Tater Tots");
+	hack.setPasswd("Taters123456");
+	hack.setPin((short) 12345);
+	hack.AcctNum(123456789);
+	hack.setBalance(1);
+	hack.deposit(01000);
+	hack.withdraw(80);
+	System.out.println(hack.toString());
 	}
 	
 	// Breaking The Bank error
