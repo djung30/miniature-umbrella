c4f41c3b2a2715c0d49a0f2f3cd9adadbe6f5ed9
Daniel Jung <djung30@stuy.edu>
2021-10-20 00:04:26 -0400

    p

--- 20_whitehat/Teller.java
+++ 20_whitehat/Teller.java
@@ -4,10 +4,12 @@ HW20: External Audit
 2021 10 19
 
 QCC:
-- What does it really mean to break an object? Does it involve run-time errors? 
+- What does it really mean to break an object? 
+- Does it involve run-time or compile errors? 
+- Should we focus on making the program trip us? 
 
 Disco:
-
+- Compile error when int is out of range (integer number too large)
 */
 
 public class Teller{
@@ -23,28 +25,28 @@ public class Teller{
 	test.deposit(4000.00);
 	test.withdraw(1000);
 	System.out.println(test.toString());
-/*
+
 	// Breaking The Bank
 	BankAccount hack = new BankAccount();
 	hack.setName("Tater Tots");
 	hack.setPasswd("Taters123456");
-	hack.setPin((short) 12345);
+	hack.setPin((short) 1234);
 	hack.setAcctNum(123456789);
 	hack.setBalance(1);
 	hack.deposit(01000);
 	hack.withdraw(80);
 	System.out.println(hack.toString());
 	}
-	*/
+
 	// Breaking The Bank error
 	/*
-	BankAccount test = new BankAccount();
-	test.setName("Tater Tots");
-	test.setPasswd("Taters123456");
-	test.setPin((short) 12345);
-	test.setAcctNum(123456789);
-	test.setBalance(600000000000000000000000000000000000000000000.00);
-	test.deposit(400000000000000000000000000000);
+	BankAccount hack2 = new BankAccount();
+	hack2.setName("Tater Tots");
+	hack2.setPasswd("Taters123456");
+	hack2.setPin((short) 1234);
+	hack2.setAcctNum(123456789);
+	hack2.setBalance(600000000000000000000000000000000000000000000.00);
+	hack2.deposit(400000000000000000000000000000);
 	test.withdraw(10000000000000000000000000000000);
 	System.out.println(test.toString());
 	*/
