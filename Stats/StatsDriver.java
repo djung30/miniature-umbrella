c928ec0486ec9051d41e00f8a848e08ec93258ef
Daniel Jung <djung30@stuy.edu>
2021-10-20 13:24:52 -0400

    Update StatsDriver.java

--- Stats/StatsDriver.java
+++ Stats/StatsDriver.java
@@ -1,3 +1,12 @@
 public class StatsDriver{ 
-  
+  public static void main(String[] args){
+    Stats test = new Stats();
+    System.out.println();
+    test.mean(1,2);
+    test.mean(1.1,1.2);
+    test.max(1,2);
+    test.max(1.1,2.2);
+    test.geoMean(1,2);
+    
+  }
 }
