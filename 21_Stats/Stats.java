cfcbe3528480ddb1e0b8a288cc853ada336570d4
Daniel Jung <djung30@stuy.edu>
2021-10-21 01:10:35 -0400

    p

--- 21_Stats/Stats.java
+++ 21_Stats/Stats.java
@@ -59,11 +59,11 @@ public class Stats {
   }
 
   public static int geoMean(int a, int b, int c) {
-    return(Math.round(Math.round(Math.pow((a * b * c), (0.333333333)))));
+    return(Math.round(Math.round(Math.pow((a * b * c), (1/3)))));
   }
   
   public static double geoMean(double a, double b, double c) {
-    return(Math.round(Math.pow((a * b * c), (0.333333333))));
+    return(Math.round(Math.pow((a * b * c), (1.3))));
   }
 
 
