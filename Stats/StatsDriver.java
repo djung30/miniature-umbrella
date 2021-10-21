e45016829e078d3f4b260cae22a9e6e32156f2ff
Daniel Jung <djung30@stuy.edu>
2021-10-21 00:53:58 -0400

    p

--- Stats/StatsDriver.java
+++ Stats/StatsDriver.java
@@ -1,3 +1,29 @@
 public class StatsDriver{ 
-  
+	public static int mean(int a, int b) {
+		return((a+b)/2);
+	}
+	public static double mean(double a, double b) {
+		return((a+b)/2); 
+	}
+	public static int max(int a, int b) {
+		if (a>b){
+			return(a);
+		}
+		else {
+			return(b);
+		}
+	}
+	public static double max(double a, double b) {
+
+	}
+	public int geoMean(int a, int b) {
+
+	}
+	public double geoMean(double a, double b) {
+
+	}
+
+	public static void main(String [] args) {
+
+	}  
 }
