76f00b5c022301a69403fcb2becfe2540953ec40
Daniel Jung <djung30@stuy.edu>
2021-10-28 21:27:42 -0400

    s

--- 27_forrevstr/Rof.java
+++ 27_forrevstr/Rof.java
@@ -6,18 +6,19 @@ QCC:
 Disco:
 */
 
-public class Rof {
-  static String fence = "|";
+public class Rof{
+
   public static String fenceF(int posts) {
+    String fence = "|";
     for (int i = 1; i < posts; i++) {
       fence += "--|";
 		}
-  	return fence;
+    return fence;
   }
-  
+
   public static void main(String[] args){
     System.out.println(fenceF(8));
     System.out.println(fenceF(1));
   }
-  
+
 }
