dd30fa881236355aeb96f17d0958012870086e0d
Daniel Jung <djung30@stuy.edu>
2021-10-28 21:34:09 -0400

    s

--- 27_forrevstr/Rof.java
+++ 27_forrevstr/Rof.java
@@ -15,10 +15,19 @@ public class Rof{
 		}
     return fence;
   }
+  
+  public static String reverseF(String s) {
+    String rs = "";
+    for (int i = s.length(); i >= 0 ; i--) {
+				rs += s.substring(i, i);
+		}
+    return rs;
+  }
 
   public static void main(String[] args){
-    System.out.println(fenceF(8));
     System.out.println(fenceF(1));
+    System.out.println(fenceF(2));
+    System.out.println(fenceF(3));
   }
 
 }
