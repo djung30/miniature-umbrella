9257663e05dcf6c4a8a2b49a0502bd40dcaeee35
Daniel Jung <djung30@stuy.edu>
2021-11-09 08:10:44 -0500

    Update Pig.java

--- lab00_oink/Pig.java
+++ lab00_oink/Pig.java
@@ -1,6 +1,80 @@
 // code to be added to your growing Pig Latin translator
 
-public class Pig {
+public class Pig
+{
+  private static final String VOWELS = "aeiou";
+
+
+  /*=====================================
+    boolean hasA(String,String) -- checks for a letter in a String
+    pre:  w != null, letter.length() == 1
+    post: hasA("cat", "a") -> true
+    hasA("cat", "p")       -> false
+    =====================================*/
+  public static boolean hasA( String w, String letter ) 
+  {
+    return w.contains(letter);
+  }//end hasA()
+
+
+  /*=====================================
+    boolean isAVowel(String) -- tells whether a letter is a vowel
+    precondition: letter.length() == 1
+    =====================================*/
+  public static boolean isAVowel( String letter ) 
+  {
+    return hasA(VOWELS, letter);
+  }
+
+
+  /*=====================================
+    int countVowels(String) -- counts vowels in a String
+    pre:  w != null
+    post: countVowels("meatball") -> 3
+    =====================================*/
+  public static int countVowels( String w ) 
+  {
+    int counter=0;
+    for (int i=0; i<w.length();i++){
+      if (isAVowel(w.substring(i,i+1))){
+        counter++;
+      }
+    }
+    return counter;
+  }
+
+
+  /*=====================================
+    boolean hasAVowel(String) -- tells whether a String has a vowel
+    pre:  w != null
+    post: hasAVowel("cat") -> true
+    hasAVowel("zzz")       -> false
+    =====================================*/
+  public static boolean hasAVowel( String w ) 
+  {
+    for (int i=0; i<w.length();i++){
+      if (isAVowel(w.substring(i,i+1))){
+        return true;
+  		}
+    }
+    return false;
+  }
+
+  /*=====================================
+    String allVowels(String) -- returns vowels in a String
+    pre:  w != null
+    post: allVowels("meatball") -> "eaa"
+    =====================================*/
+  public static String allVowels( String w ) 
+  {
+    String output = "";
+    for (int i=0; i<w.length();i++){
+      if (isAVowel(w.substring(i,i+1))){
+        output = output + w.substring(i,i+1);
+  		}
+    }
+    return output;                        
+  }
 
     private static final String VOWELS = "aeiouy";
     private static final String CAPS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
