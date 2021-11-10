2553f4c0acef7d3914b4335e1c00322669dbab69
Daniel Jung <djung30@stuy.edu>
2021-11-10 00:35:22 -0500

    Update Pig.java

--- lab00_oink/Pig.java
+++ lab00_oink/Pig.java
@@ -1,19 +1,39 @@
 /***
- * The Tator Tots (Humans: Daniel, Justin, Ari; Ducks: Ralph, Alfred)
+ * V3: add da scanner!!!!!!! + fixed hasAvowel and other tweaks
+ * Jason Zhou, Daniel Jung, Russell Goychayev (Team Dunkin' Ducks) Duckies: Tiggy, Ralph, 
  * APCS
- * HW31 -- Otnay Ybay ethay Airhay Onway Ymay Inneechay Inchay Inchay
+ * L00: Etterbay Odingcay Oughthray Ollaborationcay (Created Pig Latin translator)
  * 2021-11-09
- * QCC:
-
- * Disco: 
-
- * PRIORITIES:
- - Regular words: pog, pizza
- - Words with vowel as first letter: apple, attack
- - Words that start wwith more than one consonant: string, chug
- - Words with y: my, physics, yell
- - Words with capital letters: Hello, They
- ***/
+ * time spent: 1 hrs
+ * 
+ * DISCO: hasVowels does not work
+ * Q: n/a
+ * 
+ * HOW WE UTILIZED SCANNER DEMO in v3: 
+ *      We had it read every input and for every
+ *      input we called engToPig. We seperated each
+ *      output by spaces
+ * 
+ * 
+ * WHAT CAUSES THE RUNTIME ERROR IN THE SCANNER DEMO:
+ *      System.out.println( sc.next() );
+        System.out.println( sc.next() );
+        is the source of error. 
+        Given: 
+        Hey 
+        Boi
+        The first time main runs it see: sc has next?
+        It sees that Hey exists so it procedes. Then main prints
+        the next line so it prints out Hey, then Boi. The while loop
+        then runs again and sees that Boi is next. So it prints out sc.next.
+        However sc.next doesn't exist and you get an error. (NoSuchElementException)
+)
+        The way to fix this just to set an variable to store inputs 
+        as shown in our main
+        NEW IN v3
+ * 
+ */
+import java.util.Scanner;
 
 public class Pig
 {
@@ -80,7 +100,10 @@ public class Pig
     hasAVowel("zzz") -> false
     **/
   public static boolean hasAVowel( String w ) {
-    return w.indexOf(w) >= 0;
+    // does not work sadly 
+    // return w.indexOf(w) >= 0;
+    return allVowels(w).length() > 0;
+
   }
 
 
@@ -113,9 +136,9 @@ public class Pig
 
     String ans = "";
 
-    if ( hasAVowel(w) ) //Q: Why this necess?
+    if ( hasAVowel(w) ) {//Q: Why this necess? 
       ans = allVowels(w).substring(0,1);
-
+    }
     return ans;
   }
 
@@ -139,32 +162,54 @@ public class Pig
     engToPig("java")   --> "avajay"
     **/
   public static String engToPig( String w ) {
-
-    String ans = "";
-
-    if ( beginsWithVowel(w) )
-      ans = w + "way";
-
-    else {
-      int vPos = w.indexOf( firstVowel(w) );
-      ans = w.substring(vPos) + w.substring(0,vPos) + "ay";
+    
+    /*
+    Deals with punc, ie: pinoochio, becomes pinoochio so its easier to work w/
+    Monkey, .Pig
+    */
+    String punc = "";
+    boolean hasPunc = false;
+    if (hasPunc(w)) {
+        punc = w.substring(w.length()-1);
+        w = w.substring(0, w.length()-1);
+        
+        hasPunc = true;
     }
 
+    String ans = "";
+    //case of 1 letter 
+    if (w.length() == 1) {
+        ans = w + "ay";
+    }
+    
+    //If first letter is y, append "yay" 
+    else if ( w.substring(0, 1).equals("y") ) {
+        ans = w.substring(1) + "yay";
+    }
+    //If beings with vowel, append "way"
+    else if ( beginsWithVowel(w) ) { 
+        ans = w + "way";
+    }
+    //Capitalization for more than one letter
+    else if (hasUpper(w) && w.length() != 1) {
+        String upperCase = w.substring(1, 2).toUpperCase(); //Ex: Pig, i becomes capital
+        ans = upperCase + w.substring(2) + w.substring(0, 1).toLowerCase() + "ay";
+    } 
+    //Normal cases 
+    else {
+       
+        int vPos = w.indexOf( firstVowel(w) );
+        ans = w.substring(vPos) + w.substring(0,vPos) + "ay";
+        
+    }
+    if (hasPunc) {
+        return ans + punc;
+    }
     return ans;
   }
 
 
-  public static void main( String[] args ) {
-
-    for( String word : args ) {
-      System.out.println( "allVowels \t" + allVowels(word) );
-      System.out.println( "firstVowels \t" + firstVowel(word) );
-      System.out.println( "countVowels \t" + countVowels(word) );
-      System.out.println( "engToPig \t" + engToPig(word) );
-      System.out.println( "---------------------" );
-    }
-
-  }//end main()
+  
 
     private static final String CAPS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static final String PUNCS = ".,:;!?";
@@ -195,8 +240,8 @@ public class Pig
     /*=====================================
       boolean hasPunc(String) -- tells whether a String contains punctuation
       pre:  w != null
-      post: hasPunc(“cat.”) -> true
-            hasPunc(“cat”) -> false
+      post: hasPunc("cat.") -> true
+            hasPunc("cat") -> false
       =====================================*/
     public static boolean hasPunc( String w ) {
       for (int i=0; i<w.length();i++){
@@ -215,7 +260,16 @@ public class Pig
             beginsWithUpper("apple") -> false
       =====================================*/
     public static boolean beginsWithUpper( String w ) {
-      return isUpperCase(w.substring(0,1) );
+        return isUpperCase(w.substring(0,1) );
+    }
+
+    public static boolean hasUpper( String w ) {
+        for (int i=0; i<w.length();i++){
+            if (beginsWithUpper(w.substring(i,i+1))){
+              return true;
+            }
+        }
+        return false;
     }
 
 /*
@@ -229,5 +283,23 @@ public class Pig
     System.out.println(beginsWithUpper("Apple")); // -> true
     System.out.println(beginsWithUpper("apple")); // -> false
   }
+  
 */
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner( System.in );
+        boolean quit = true;
+        while (scanner.hasNext() && quit) {
+            String input = scanner.next();
+            if (input.equals("iwanttoquitthis")) {
+                quit = false;
+            }
+            System.out.print(engToPig(input) + " ");
+            
+        }
+
+        
+        
+
+    }
 }//end class Pig
